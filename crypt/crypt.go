04e906a43c07476e200503b88d686838caacced6
Steve Phillips <elimisteve@gmail.com>
2013-01-12 10:36:41 -0800

    Added utils/gobs.go (for Gob encoding/decoding), crypt/crypt.go (for AES encryption/decryption)

---
+++ crypt/crypt.go
@@ -0,0 +1,53 @@
+package crypt
+
+import (
+	// "crypto/aes"
+	"crypto/cipher"
+	"fmt"
+	"log"
+)
+
+func AESEncryptBytes(block cipher.Block, data []byte) (cipher []byte, err error) {
+	blockSize := block.BlockSize()
+
+	length := len(data)
+
+	cipherBytes := make([]byte, length)
+	numBlocks := length / blockSize
+	// Add one more if there were bytes left over
+	if length % blockSize != 0 {
+		numBlocks++
+	}
+
+	// Encrypt
+	for i := 0; i < length; i += blockSize {
+		block.Encrypt(cipherBytes[i:i+blockSize], data[i:i+blockSize])
+	}
+
+	return cipherBytes, nil
+}
+
+func AESDecryptBytes(block cipher.Block, cipherBytes []byte) (plain []byte, err error) {
+	defer func() {
+		if e := recover(); e != nil {
+			log.Printf("Panic from AESDecryptBytes: %v", e)
+			plain = nil
+			err = fmt.Errorf("%v", e)
+		}
+	}()
+
+	blockSize := block.BlockSize()
+	plain = make([]byte, len(cipherBytes))
+	for i := 0; i < len(cipherBytes); i += blockSize {
+		block.Decrypt(plain[i:i+blockSize], cipherBytes[i:i+blockSize])
+	}
+	return plain, nil
+}
+
+func PadBytes(data []byte, blockSize int) []byte {
+	// Add padding (originally for correctness, now for simplicity)
+	for len(data) % blockSize != 0 {
+		data = append(data, 0x0)
+	}
+	return data
+}
