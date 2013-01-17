0655904dac5e6825da00027e06509c942ef54af1
Steve Phillips <elimisteve@gmail.com>
2013-01-16 23:06:33 -0800

    Added PadString to pad.go

--- pad.go
+++ pad.go
@@ -2,8 +2,15 @@ package utils
 
 func PadBytes(data []byte, blockSize int) []byte {
 	// Add padding (originally for correctness, now for simplicity)
-	for len(data) % blockSize != 0 {
+	for len(data) % blockSize == 1 {
 		data = append(data, 0x0)
+	}
+	return data
+}
+
+func PadString(data string, multiple int) string {
+	for len(data) % multiple == 1 {
+		data += string(0x0)
 	}
 	return data
 }
